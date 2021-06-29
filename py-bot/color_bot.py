8c654670534822a41560b3a9a4aff9a8d88ae316
chhollenbach <hollenbach256@gmail.com>
2021-06-29 17:54:13 -0500

    minor tweaks

--- py-bot/color_bot.py
+++ py-bot/color_bot.py
@@ -14,22 +14,25 @@ def main():
     # create a stream of reddit comments
     subreddits = reddit.subreddit("all")
     for comment in subreddits.stream.comments():
-        process_comment(comment, color_list)
+        comment_color_info = process_comment(comment, color_list)
+        for color_data in comment_color_info:
+            print(color_data)
 
 
 def process_comment(comment, colors):
     """
-    Helper function that scans a text comment for any mention of a color, and returns data if found
+    Helper function that scans a text comment for any mention of a color. Returns a list containing
+    lists if info i
     """
     color_info = []
     for color in colors:
         if ' ' + color + ' ' in comment.body.lower():
             if color == 'purple':
-                color_info.append([comment.created_utc, 'violet', comment.subreddit_id])
-                print("violet")
+                color_info.append([comment.created_utc, 'violet', comment.subreddit.display_name])
             else:
-                color_info.append([comment.created_utc, color, comment.subreddit_id])
-                print(color)
+                color_info.append([comment.id, comment.created_utc, color, comment.subreddit.display_name])
+
+    return color_info
 
 if __name__ == "__main__":
     main()
