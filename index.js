df61ccdc2b2b3b55dc6cb6ed962f2040144d3623
chhollenbach <hollenbach256@gmail.com>
2021-08-02 17:06:15 -0500

    connect to heroku postgresql

--- index.js
+++ index.js
@@ -9,8 +9,35 @@ const pass = process.env.PASS;
 var users = {};
 users[user] = pass;
 
-app.listen(3000, () => {
- console.log("Server running on port 3000: http://localhost:3000");
+// DATABASE URL for Heroku postgresql - not needed when deploying
+const DATABASE_URL = process.env.DATABASE_URL;
+const { Client } = require('pg')
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+
+client.connect();
+
+// query objects
+const dropTableQuery = 'DROP TABLE IF EXISTS reddit_rainbow_raw';
+const createTableQuery = 'CREATE TABLE reddit_rainbow_raw (' + 
+                'id                      SERIAL PRIMARY KEY,' +
+                'comment_id              VARCHAR(100) NOT NULL,' +
+                'created_utc             integer NOT NULL,' + 
+                'color                   VARCHAR(100) NOT NULL,' +
+                'subreddit_display_name  VARCHAR(100) NOT NULL' +
+                ');'
+const getFirstRowQuery = 'SELECT * FROM reddit_rainbow_raw LIMIT 1;';
+const insertRowQuery = 'INSERT INTO reddit_rainbow_raw (comment_id, created_utc, color, subreddit_display_name) VALUES ($1, $2 ,$3 ,$4);';
+
+
+// Heroku Port or Local port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
 
 
@@ -18,13 +45,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.get("/", (req, res, next) => {
-    res.json({message: 'hello world! Reddit rainbow API'})
-   });
+// app.get("/reset_table", (req, res, next) => {
+//     client.query(dropTableQuery, function(err){
+//         client.query(createTableQuery, function(err){
+//           console.log('Table Reset');
+//         })
+//       });
+//    });
 
 app.post("/pybot_data", basicAuth({
     users,
     unauthorizedResponse: "valid credentials required"}), (req, res, next) => {
-            console.log(req.body)
-            res.send(req.body)
+        var {comment_id, created_utc, color, subreddit_display_name} = req.body;
+        client.query(insertRowQuery, [comment_id, created_utc, color, subreddit_display_name], (err, result) => {
+            if(err){
+                next(err)
+                return
+            };
+          })
+        res.send(req.body)
+        console.log(req.body)
    });
